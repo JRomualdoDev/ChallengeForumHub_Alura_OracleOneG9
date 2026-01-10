3e6ac5f699cfc9d7e549c6788e9ddaa6325567c8
JoseRomualdo <jromualdo3@hotmail.com>
2026-01-10 10:56:58 -0300

    feat(Perfil): add entity perfil.

---
+++ src/main/java/br/com/alura/forumhub/domain/perfil/Perfil.java
@@ -0,0 +1,21 @@
+package br.com.alura.forumhub.domain.perfil;
+
+import br.com.alura.forumhub.domain.topico.Topico;
+import br.com.alura.forumhub.domain.usuario.Usuario;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Perfil {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToMany(mappedBy = "perfis")
+    private Set<Usuario> usuarios;
+
+}
