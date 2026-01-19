375220a661f2708bf0533ab1e0512a4bcb668b26
JoseRomualdo <jromualdo3@hotmail.com>
2026-01-19 14:45:48 -0300

    refactor(Curso): add annotation lombok.

--- src/main/java/br/com/alura/forumhub/domain/curso/Curso.java
+++ src/main/java/br/com/alura/forumhub/domain/curso/Curso.java
@@ -2,19 +2,28 @@ package br.com.alura.forumhub.domain.curso;
 
 import br.com.alura.forumhub.domain.topico.Topico;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Setter @Getter
+@RequiredArgsConstructor
 public class Curso {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
+    @NotNull
     private String nome;
+    @NotNull
     private String categoria;
 
     @OneToMany(mappedBy = "curso")
-    private Set<Topico> topicos;
+    private Set<Topico> topicos = new HashSet<>();
 }
