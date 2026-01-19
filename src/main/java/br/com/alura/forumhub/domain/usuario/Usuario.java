5e9cd8db14e620790f9bcd13ed01da8a13b06678
JoseRomualdo <jromualdo3@hotmail.com>
2026-01-19 14:47:46 -0300

    refactor(Topico): add annotations lombok and method userDetails.

--- src/main/java/br/com/alura/forumhub/domain/usuario/Usuario.java
+++ src/main/java/br/com/alura/forumhub/domain/usuario/Usuario.java
@@ -4,25 +4,37 @@ import br.com.alura.forumhub.domain.perfil.Perfil;
 import br.com.alura.forumhub.domain.resposta.Resposta;
 import br.com.alura.forumhub.domain.topico.Topico;
 import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
-public class Usuario {
+@Setter @Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     private String nome;
+    @NonNull
     private String email;
+    @NonNull
     private String senha;
 
-    @OneToMany
-    private Set<Topico> topicos;
+    @OneToMany(mappedBy = "autor")
+    private Set<Topico> topicos = new HashSet<>();
 
-    @OneToMany
-    private Set<Resposta> respostas;
+    @OneToMany(mappedBy = "autor")
+    private Set<Resposta> respostas = new HashSet<>();
 
     @ManyToMany
     @JoinTable(
@@ -30,5 +42,40 @@ public class Usuario {
             joinColumns = @JoinColumn(name = "usuario_id"),
             inverseJoinColumns = @JoinColumn(name = "perfil_id")
     )
-    private Set<Perfil> perfis;
+    private Set<Perfil> perfis = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return nome;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
