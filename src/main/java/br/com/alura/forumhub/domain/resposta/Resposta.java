8275a2ed9f6489f7b07aa53bfc7be93a176a457b
JoseRomualdo <jromualdo3@hotmail.com>
2026-01-22 10:16:02 -0300

    refactor(Resposta): add annotation lombok.

--- src/main/java/br/com/alura/forumhub/domain/resposta/Resposta.java
+++ src/main/java/br/com/alura/forumhub/domain/resposta/Resposta.java
@@ -3,27 +3,35 @@ package br.com.alura.forumhub.domain.resposta;
 import br.com.alura.forumhub.domain.topico.Topico;
 import br.com.alura.forumhub.domain.usuario.Usuario;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter @Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Resposta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     private String mensagem;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "topico_id", nullable = false)
     private Topico topico;
 
-    private LocalDateTime dataCriacao;
+    private LocalDateTime dataCriacao = LocalDateTime.now();
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario autor;
 
-    private String solucao;
+    @NonNull
+    private Boolean solucao;
 }
