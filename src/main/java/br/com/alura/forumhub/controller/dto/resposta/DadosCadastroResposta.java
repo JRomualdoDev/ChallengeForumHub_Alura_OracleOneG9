4dd8b5fa78011b5a4a8be5c4e0e3571ece9464ff
JoseRomualdo <jromualdo3@hotmail.com>
2026-01-26 07:57:32 -0300

    refactor(DadosCadastroResposta): add non null.

--- src/main/java/br/com/alura/forumhub/controller/dto/resposta/DadosCadastroResposta.java
+++ src/main/java/br/com/alura/forumhub/controller/dto/resposta/DadosCadastroResposta.java
@@ -1,8 +1,14 @@
 package br.com.alura.forumhub.controller.dto.resposta;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+
 public record DadosCadastroResposta(
+        @NotNull
         String mensagem,
+        @NonNull
         Long topicoId,
+        @NotNull
         boolean solucao
 ) {
 }
