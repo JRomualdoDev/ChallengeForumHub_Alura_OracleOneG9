8b8a58b21317a377376f9d516c96477e577f3b16
JoseRomualdo <jromualdo3@hotmail.com>
2026-01-26 07:55:51 -0300

    feat(DadosAutenticacao): new file.

--- src/main/java/br/com/alura/forumhub/controller/dto/autenticacao/DadosAutenticacao.java
+++ src/main/java/br/com/alura/forumhub/controller/dto/autenticacao/DadosAutenticacao.java
@@ -1,4 +1,11 @@
 package br.com.alura.forumhub.controller.dto.autenticacao;
 
-public record DadosAutenticacao(String email, String senha) {
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAutenticacao(
+        @NotNull
+        String email,
+        @NotNull
+        String senha
+) {
 }
