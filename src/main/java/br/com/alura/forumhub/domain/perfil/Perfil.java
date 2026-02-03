2bb7f69dea73beb06e9d64d3c30682a59765a5c0
JoseRomualdo <jromualdo3@hotmail.com>
2026-02-03 09:56:02 -0300

    refactor(Perfil): add equals, hashcode and getAuthority.

--- src/main/java/br/com/alura/forumhub/domain/perfil/Perfil.java
+++ src/main/java/br/com/alura/forumhub/domain/perfil/Perfil.java
@@ -3,12 +3,14 @@ package br.com.alura.forumhub.domain.perfil;
 import br.com.alura.forumhub.domain.topico.Topico;
 import br.com.alura.forumhub.domain.usuario.Usuario;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
-public class Perfil {
+public class Perfil implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +21,19 @@ public class Perfil {
     @ManyToMany(mappedBy = "perfis")
     private Set<Usuario> usuarios = new HashSet<>();
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Perfil perfil = (Perfil) o;
+        return id != null && id.equals(perfil.id);    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.nome;
+    }
 }
