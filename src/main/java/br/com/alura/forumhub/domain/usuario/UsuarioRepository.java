7dcff0f558c8984d0a0c493fcd0084e2085801cf
JoseRomualdo <jromualdo3@hotmail.com>
2026-02-03 09:58:52 -0300

    refactor(UsuarioRepository): add findAll.

--- src/main/java/br/com/alura/forumhub/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/alura/forumhub/domain/usuario/UsuarioRepository.java
@@ -1,11 +1,16 @@
 package br.com.alura.forumhub.domain.usuario;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
     UserDetails findByEmail(String subject);
+
+    Page<Usuario> findAll(Pageable pageable);
 }
